0af1aac94eb8c6c15eb9003e6840657c9f8c63e3
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2024-05-05 23:56:29 +0530

    completed and fixed

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from PIL import Image
 from transformers import VisionEncoderDecoderModel, ViTImageProcessor, GPT2TokenizerFast
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = 'static/uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -21,10 +21,20 @@ image_processor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-c
 
 def process_image(image_path):
     image = Image.open(image_path)
+    print("Image format:", image.format)
+    print("Image mode:", image.mode)
+    print("Image size:", image.size)
+    
+    # Convert image to RGB mode
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+    
     img = image_processor(image, return_tensors="pt").to(device)
     output = model.generate(**img)
     caption = tokenizer.batch_decode(output, skip_special_tokens=True)[0]
     return caption
+
+
 
 def generate_audio(caption):
     tts = gTTS(text=caption, lang='en')
@@ -33,9 +43,16 @@ def generate_audio(caption):
     audio_io.seek(0)
     return audio_io
 
+def save_audio_to_file(audio_io, filename):
+    with open(filename, "wb") as f:
+        f.write(audio_io.read())
+        audio_io.close()  # Close the BytesIO object after writing
+    print("Audio file saved to:", filename)
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('upload.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -46,19 +63,26 @@ def upload_file():
         if photo.filename == '':
             return redirect(request.url)
         if photo:
+            print("GOT PHOTO")
             filename = photo.filename
             photo_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             photo.save(photo_path)
             # Process the uploaded image
             caption = process_image(photo_path)
+            print("CAPTION GENERATED")
+            print(caption)
             # Generate audio from the caption
             audio_io = generate_audio(caption)
-            # Convert audio format if needed
-            audio = AudioSegment.from_file(audio_io, format="mp3")
-            audio_path = os.path.join(app.config['UPLOAD_FOLDER'], 'caption_audio.mp3')
-            audio.export(audio_path, format="mp3")
-            return render_template('result.html', caption=caption, audio_path=audio_path)
-    return render_template('upload.html')
+            print("AUDIO GENERATED")
+            # Save audio to a temporary file
+            audio_temp_path = os.path.join(app.config['UPLOAD_FOLDER'], f'{os.path.splitext(filename)[0]}.mp3')
+            save_audio_to_file(audio_io, audio_temp_path)
 
+            return render_template('result.html', caption=caption, audio_path=audio_temp_path)
+
+    return redirect(url_for('index'))
+
+
+        
 if __name__ == '__main__':
     app.run(debug=True)
